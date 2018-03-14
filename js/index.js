05ba2506a37fdec852009c85fa1565d864eccdbc
adershmanoj <adershmanoj@gmail.com>
2018-03-14 21:27:08 +0530

    Changed styles

--- js/index.js
+++ js/index.js
@@ -2,21 +2,42 @@ var page = document.getElementById('page');
 var last_pane = page.getElementsByClassName('pane');
 last_pane = last_pane[last_pane.length-1];
 var dummy_x = null;
-var offset = 900;
-
+var offset = 1000;
+var h = window.innerHeight * 2;
 var isMobile = window.matchMedia("only screen and (max-width: 760px)");
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		scrollBar: true,
+		normalScrollElements: '#page'
+	});
+	resize();
+	window.onresize = resize;
+
+	// Reset window-based vars
+	function resize() {
+		var w = page.scrollWidth-window.innerWidth+window.innerHeight-2000;
+		if(!isMobile.matches)
+			w += (offset+2600);
+		document.body.style.height = w + 'px';
+	}
+});
 window.onscroll = function () {
   // Horizontal Scroll.
   if(!isMobile.matches){
     var y = window.scrollY;
-    if(y > 900){
+		console.log(y)
+    if(y > h+60){
       page.scrollLeft = y - offset;
       page.style.position = 'fixed';
+			$('#footer').css('position', 'fixed');
+			
     }
     else{
       offset = window.scrollY;
       page.scrollLeft = 0;
       page.style.position = 'relative';
+			$('#footer').css('position', 'absolute');
+
     }
   }
   else{
@@ -24,14 +45,4 @@ window.onscroll = function () {
   }
 }
 // Adjust the body height if the window resizes.
-window.onresize = resize;
 // Initial resize.
-resize();
-
-// Reset window-based vars
-function resize() {
-  var w = page.scrollWidth-window.innerWidth+window.innerHeight-2000;
-  if(!isMobile.matches)
-    w += (offset+2000);
-  document.body.style.height = w + 'px';
-}
